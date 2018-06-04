197b246d38f1ed1d1d46df8568378805767d6e32
snekasriDLN <snekasri777@gmail.com>
2018-06-04 06:49:46 +0530

    Animal

---
+++ Animal.java
@@ -0,0 +1,30 @@
+package com.java;
+
+public abstract class Animal implements Travel {
+	
+}
+class Dog extends Animal{
+	@Override
+	public int getspeed() {
+		return 100;
+	}
+}
+class Rat extends Animal{
+	@Override
+	public int getspeed() {
+		return 100;
+	}
+}
+class Lion extends Animal{
+	@Override
+	public int getspeed() {
+		return 100;
+	}
+}
+	
+
+
+	
+
+
+
